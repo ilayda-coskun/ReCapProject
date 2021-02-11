90c2cd788cab0ad99ccd2c8ed64039db64c3ccf2
İlayda Coşkun <ilayda.coskun2001@hotmail.com>
2021-02-11 14:53:44 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Console.WriteLine("---------KİRALIK ARABA LİSTESİ---------\n");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine( " Arabanın Modeli : " + car.Description + 
+                    "\n Arabanın Üretim Yılı :  " + car.ModelYear + 
+                    "\n Ararabanın Günlük Kira Fiyatı :  " + car.DailyPrice +
+                    "\n----------------------------------------");
+            }
+        }
+    }
+}
